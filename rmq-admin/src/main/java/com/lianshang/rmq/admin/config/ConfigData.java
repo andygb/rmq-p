c4599dcf4c023b744e3a15b9311ee03327da509b
yuan.zhong <yuan.zhong@dianping.com>
2016-03-30 10:56:58 +0800

    cat

--- rmq-admin/src/main/java/com/lianshang/rmq/admin/config/ConfigData.java
+++ rmq-admin/src/main/java/com/lianshang/rmq/admin/config/ConfigData.java
@@ -1,13 +1,19 @@
 package com.lianshang.rmq.admin.config;
 
+import com.dianping.cat.servlet.CatFilter;
 import com.lianshang.rmq.admin.security.SecurityInterceptor;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.MultipartConfigFactory;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterConfig;
 import javax.servlet.MultipartConfigElement;
 
 /**
@@ -18,6 +24,16 @@ import javax.servlet.MultipartConfigElement;
 @ImportResource("classpath*:config/spring/*.xml")
 public class ConfigData extends WebMvcConfigurerAdapter {
 
+    @Bean
+    public Filter characterEncodingFilter() {
+        return new CharacterEncodingFilter("UTF-8");
+    }
+
+    @Bean
+
+    public Filter catFilter() {
+        return new CatFilter();
+    }
 
     @Bean
     public MultipartConfigElement multipartConfigElement() {
